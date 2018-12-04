f6e304299b64f59a43f2df7f34954de1faca8670
Camilo Buelvas <rcbuelvasv@unal.edu.co>
2018-12-04 01:25:00 +0000

    SOAP 1

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'mongoid', '~> 6.1.0'
 gem 'bson_ext'
+
+gem 'wash_out'
---
+++ app/controllers/wsauthors_controller_controller.rb
@@ -0,0 +1,30 @@
+class WsauthorsControllerController < ApplicationController
+    before_action :set_author, only: [:show, :update, :destroy]
+    
+    soap_service namespace: 'urn:WashOutAuthors', camelize_wsdl: :lower
+
+    soap_action "index",
+                :return => :authors
+    def index
+      @authors = Author.all
+      render :soap => @authors
+    end
+    
+    soap_action "show",
+                :args   => { :username => :string},
+                :return => :author
+    def show
+        render :soap => @author
+    end
+  
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_author
+          @author = Author.where(username: params[:id])
+        end
+    
+        # Only allow a trusted parameter "white list" through.
+        def author_params
+          params.require(:author).permit(:username, :name, :projects)
+        end
+end
